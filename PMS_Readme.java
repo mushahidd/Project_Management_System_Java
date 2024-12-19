41f007dd82e7e9ec9c0327ce8dafec40c7a06b1e
Mushahid Hussain <mushahidhussain451@gmail.com>
2024-12-20 02:22:59 +0500

    Add files via upload

---
+++ PMS_Readme.java
@@ -0,0 +1,24 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("A semester project (ERP) using Java and MySql
+
+-> A very simple project in java which is Enterprise Resource Planning, connected with database (MySql) local host phpmyadmin.
+
+-> First you should have these 2 software "Netbeans & XAMPP" to run this project.
+
+-> Now download the .rar file above (all files are in .rar folder no need to download the readme.java file).
+
+-> Now start XAMPP and run Apache with MySql.
+
+-> Now go to web http://localhost/phpmyadmin and then create a database name erp after creating database in phpmyadmin follow next step.
+
+-> Now go to source package then in (dao) folder go to table file then uncomment all the lines which are commented then run the table file once, recomment those lines after running the file.
+
+-> Then run the project and login by entering (admin) both in Id & password.
+
+-> Now explore the functionality of this project and enjoy.
+
+-> This project has very limited functions compare to the professional ERP system.");
+    }
+}
