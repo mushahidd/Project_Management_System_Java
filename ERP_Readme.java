e23a58167ad96f8c6d568b6069c1b2cc0cf0b226
Mushahid Hussain <mushahidhussain451@gmail.com>
2024-12-20 02:16:49 +0500

    Add files via upload

---
+++ ERP_Readme.java
@@ -0,0 +1,22 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("A semester project (PMS) using Java and MySql
+
+-> A very simple project in java which is Project Management System in which we can upload our projects and can save them and also can download the saved project with CRUD functionality, connected with database (MySql) local host phpmyadmin.
+
+-> First you should have these 2 software "Netbeans & XAMPP" to run this project.
+
+-> Now start XAMPP and run Apache with MySql.
+
+-> Now go to web http://localhost/phpmyadmin and then create a database name dbms (i dont remember that what to name the DB but if dbms doesn't works so go to the source package then dao folder then in "connection provider" file in which you can see what to name the database) after creating database in phpmyadmin follow next step.
+
+-> Now go to source package then in (dao) folder then go to "table file" then uncomment all the lines which are commented then run the table file once, recomment those lines after running the file.
+
+-> Then run the project and login by entering (admin12) both in Id & password. (If admin12 does not work try admin)
+
+-> Now explore the functionality of this project and enjoy.
+
+-> This project has very limited functions compare to the professional ERP system.");
+    }
+}
